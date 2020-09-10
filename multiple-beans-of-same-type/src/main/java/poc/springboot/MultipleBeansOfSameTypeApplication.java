cd66db9c7554335d7ede20a2e174bdb156fa59a0
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-10 13:31:01 +0530

    multiple-beans-of-same-type: Multiple beans of same type: Worked

--- multiple-beans-of-same-type/src/main/java/poc/springboot/MultipleBeansOfSameTypeApplication.java
+++ multiple-beans-of-same-type/src/main/java/poc/springboot/MultipleBeansOfSameTypeApplication.java
@@ -1,13 +1,34 @@
 package poc.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+import poc.springboot.domain.Person;
+
+@Slf4j
 @SpringBootApplication
 public class MultipleBeansOfSameTypeApplication {
+
+	@Autowired
+	private Person abPerson;
+	@Autowired
+	private Person sbPerson;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultipleBeansOfSameTypeApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ac) {
+		return args -> {
+			log.debug("abPerson: " + abPerson);
+			log.debug("sbPerson: " + sbPerson);
+		};
+	}
 }
